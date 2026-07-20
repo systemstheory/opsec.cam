c2df0731243dcfb144364739ab937d12890df31d
systemstheory <system5theory@protonmail.com>
2026-07-21 00:39:43 +0200

    Placeholder

--- website/src/lib/assets/AboutIcon.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import Icon from './Icon.svelte';
-	import frame0 from '../../../static/about1.svg?raw';
-	import frame1 from '../../../static/about2.svg?raw';
-	let { index } = $props();
-</script>
-
-<Icon frames={[frame0, frame1]} {index} />
--- website/src/lib/assets/PhotosIcon.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import Icon from './Icon.svelte';
-	import frame0 from '../../../static/photos1.svg?raw';
-	import frame1 from '../../../static/photos2.svg?raw';
-	let { index } = $props();
-</script>
-
-<Icon frames={[frame0, frame1]} {index} />
